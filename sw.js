717bb29728e271e9e8865391fe32ce3a2a789931
basana <a.basin@gmx.de>
2026-09-20 22:12:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'rhine-design-test-v11'; // при каждом деплое с изменениями увеличивайте v5 -> v6 -> v7 ...
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
